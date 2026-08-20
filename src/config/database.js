d6dfb836f7503209f403e818a11d8698d9b013aa
Anukriti Gupta <anukritigupta12@outlook.com>
2026-08-20 23:11:04 +0530

    MongoDB Connection

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,14 @@
+const dns = require('dns');
 const mongoose = require('mongoose');
+
+dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 async function connectDB() {
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected');
   } catch (err) {
-    console.error(err.message);
+    console.error('MongoDB connection failed:', err.message);
     process.exit(1);
   }
 }
