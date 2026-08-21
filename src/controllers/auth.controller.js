3e68fc33d074f3d79150ecc8d02471fa94e023ff
Anukriti Gupta <anukritigupta12@outlook.com>
2026-08-21 23:25:12 +0530

    register and Login created succesfully

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -27,11 +27,50 @@ async function registerUserController(req, res) {
     const User = new userModel.create({ username, email, password: hash });
 
     const token = jwt.sign({ id: user._id , username : user.username  }, process.env.JWT_SECRET, { expiresIn: '1d' });
-    res.cookie('token', token, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 }); // 1 day
-    res.status(201).json({ message: 'User registered successfully', token });
+    res.cookie('token', token ); // 1 day
+    res.status(201).json({ message: 'User registered successfully',
+        user: { id: user._id, username: user.username, email: user.email }, 
+     });
     
 }
 
+/**
+ * @name loginUserController
+ * @description Login a user expecting email and password in the request body
+ * @access public
+ * 
+ * 
+ * 
+ */
+
+async function loginUserController(req, res) {
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email });
+    if (!user) {
+        return res.status(400).json({ 
+            message: 'Invalid email or password' 
+        });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+        return res.status(400).json({ 
+            message: 'Invalid email or password' 
+        });
+    }
 
 
- module.exports = { registerUserController };
+    const token = jwt.sign({ id: user._id , username : user.username  }, process.env.JWT_SECRET, { expiresIn: '1d' });  
+     
+    res.cookie('token', token ); // 1 day
+    res.status(200).json({ message: 'User loggedIn successfully',
+        user: { 
+        id: user._id, 
+        username: user.username,
+        email: user.email 
+    },
+})
+
+
+}
+ module.exports = { registerUserController , loginUserController };
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -11,4 +11,12 @@ const authRouter = express.Router();
  authRouter.post('/register' , authController.registerUserController);
 
 
+ /**
+ * @route POST /api/auth/login
+ * @description Login a user with email and password
+ * @access public
+ */
+  authRouter.post('/login' , authController.loginUserController);
+
+
 module.exports = authRouter;
