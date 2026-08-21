caa81c26d5cde5e3bc98f474c6c22f5915f1a62b
Anukriti Gupta <anukritigupta12@outlook.com>
2026-08-21 23:03:45 +0530

    Hashing

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,6 @@
 const userModel = require('../models/user.model');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 /**
  * @route POST /api/auth/register
@@ -19,7 +21,17 @@ async function registerUserController(req, res) {
         $or : [{ username }, { email }] })
     if (isUserAlreadyExists) {
         return res.status(400).json({ message: 'User already exists' });
+    
     }
+    const hash = await bcrypt.hash(password, 10);
+    const User = new userModel.create({ username, email, password: hash });
+
+    const token = jwt.sign({ id: user._id , username : user.username  }, process.env.JWT_SECRET, { expiresIn: '1d' });
+    res.cookie('token', token, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 }); // 1 day
+    res.status(201).json({ message: 'User registered successfully', token });
+    
 }
+
+
 
  module.exports = { registerUserController };
