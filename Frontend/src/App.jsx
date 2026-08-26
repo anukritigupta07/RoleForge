c67f51059e68f46200da20e5b3dc7b99b31b1c88
Anukriti Gupta <anukritigupta12@outlook.com>
2026-08-26 19:19:18 +0530

    Login and Register Pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,15 @@
+import { RouterProvider } from "react-router"
+import { router } from "./app.routes.jsx"
+import { AuthProvider } from "./features/auth/auth.context.jsx"
+
 function App() {
-   return (
-    <>
-    </>
+
+  return (
+    <AuthProvider>
+      
+        <RouterProvider router={router} />
+      
+    </AuthProvider>
   )
 }
 
