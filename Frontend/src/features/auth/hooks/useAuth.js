8af08788f68c427a3cc8d3965b508ac58736803c
Anukriti Gupta <anukritigupta12@outlook.com>
2026-09-06 21:24:47 +0530

    Interview Frontend

--- Frontend/src/features/auth/hooks/useAuth.js
+++ Frontend/src/features/auth/hooks/useAuth.js
@@ -28,6 +28,7 @@ export const useAuth = () => {
             const data = await register({ username, email, password })
             setUser(data.user)
         } catch (err) {
+            
 
         } finally {
             setLoading(false)
