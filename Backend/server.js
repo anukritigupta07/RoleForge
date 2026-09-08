277c25446ef8249b6205a0e9ac038b908838727a
Anukriti Gupta <anukritigupta12@outlook.com>
2026-09-08 23:47:48 +0530

    AI SERVICES IN BACKEND IS COMPLETED

--- Backend/server.js
+++ Backend/server.js
@@ -2,8 +2,10 @@ require('dotenv').config();
 
 const app= require('./src/app');
 const connectDB = require('./src/config/database.js');
+//const invokeGeminiAi = require('./src/services/ai.service.js');  
 
 connectDB();
+//invokeGeminiAi();
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
