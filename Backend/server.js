adfdeaf6a75c2efcdc95be922186fe4ad24876dc
Anukriti Gupta <anukritigupta12@outlook.com>
2026-09-09 23:46:40 +0530

    Frontend Integration of AI

--- Backend/server.js
+++ Backend/server.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 
 const app= require('./src/app');
 const connectDB = require('./src/config/database.js');
-//const invokeGeminiAi = require('./src/services/ai.service.js');  
+const invokeGeminiAi = require('./src/services/ai.service.js');  
 
 connectDB();
-//invokeGeminiAi();
+invokeGeminiAi();
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
